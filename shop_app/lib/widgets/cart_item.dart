63149574ba7c9b6c5e3e2845e24eb3ed05125ca4
Krzysztof Gerc <krzysztofgerc10@gmail.com>
2021-12-04 14:09:45 +0100

    Added snackbar when adding item to the cart

--- shop_app/lib/widgets/cart_item.dart
+++ shop_app/lib/widgets/cart_item.dart
@@ -33,7 +33,7 @@ class CartItem extends StatelessWidget {
           padding: EdgeInsets.only(right: 20)),
       direction: DismissDirection.endToStart,
       onDismissed: (direction) {
-        Provider.of<Cart>(context, listen: false).removeitem(productId);
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
       child: Card(
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
